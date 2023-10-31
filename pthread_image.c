7d08a3d3c33fc738ad3753f484582aa4cc3689b6
christiankuhnn <kuhnc@udel.edu>
2023-10-30 22:32:30 -0400

    hello

--- pthread_image.c
+++ pthread_image.c
@@ -2,62 +2,51 @@
 #include <stdint.h>
 #include <time.h>
 #include <string.h>
-#include <pthread.h>
 #include "image.h"
+#include <pthread.h>
+
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-#include "stdlib.h"
 
-#define NUM_THREADS 4
+#define NUM_THREADS 10
 
-Matrix algorithms[] = {
-    {{0, -1, 0}, {-1, 4, -1}, {0, -1, 0}},
-    {{0, -1, 0}, {-1, 5, -1}, {0, -1, 0}},
-    {{1/9.0, 1/9.0, 1/9.0}, {1/9.0, 1/9.0, 1/9.0}, {1/9.0, 1/9.0, 1/9.0}},
-    {{1.0/16, 1.0/8, 1.0/16}, {1.0/8, 1.0/4, 1.0/8}, {1.0/16, 1.0/8, 1.0/16}},
-    {{-2, -1, 0}, {-1, 1, 1}, {0, 1, 2}},
-    {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}
-};
+typedef struct inputStruct {
+  Image* srcImage;
+  Image* destImage;
+  enum KernelTypes type;
+  long rank;
+} inputStruct;
 
-typedef struct {
-    int thread_id;
-    Image* srcImage;
-    Image* destImage;
-    Matrix algorithm;
-} ThreadArgs;
+void *threaded_convolute(void *args){
+  int row, pix, bit, span;
+  inputStruct* input = (inputStruct*)args;
+  int localHeight = input->srcImage->height / NUM_THREADS;
+  int localHeightRemainder = input->srcImage->height % NUM_THREADS;
+  int start_row = input->rank * localHeight;
+  int end_row = (input->rank + 1) * localHeight;
+  if (input->rank == NUM_THREADS - 1)
+    end_row = end_row + localHeightRemainder;
 
-void* threadConvolute(void* args) {
-    ThreadArgs* threadArgs = (ThreadArgs*)args;
-    int thread_id = threadArgs->thread_id;
-    Image* srcImage = threadArgs->srcImage;
-    Image* destImage = threadArgs->destImage;
-    enum KernelTypes type = threadArgs->type; // Pass the type as an argument
-
-    // Access the appropriate algorithm matrix from the algorithms array
-    Matrix algorithm = algorithms[type];
-
-    int start_row = thread_id * (srcImage->height / NUM_THREADS);
-    int end_row = (thread_id == (NUM_THREADS - 1)) ? srcImage->height : (start_row + (srcImage->height / NUM_THREADS));
-
-    for (int row = start_row; row < end_row; row++) {
-        for (int pix = 0; pix < srcImage->width; pix++) {
-            for (int bit = 0; bit < srcImage->bpp; bit++) {
-                destImage->data[Index(pix, row, srcImage->width, bit, srcImage->bpp)] =
-                    getPixelValue(srcImage, pix, row, bit, algorithm);
-            }
-        }
+  for (row = start_row; row < end_row; row++) {
+    for (pix = 0; pix < input->srcImage->width; pix++) {
+      for (bit = 0; bit < input->srcImage->bpp; bit++) {
+        input->destImage->data[Index(pix, row, input->srcImage->width, bit, input->srcImage->bpp)] =
+            getPixelValue(input->srcImage, pix, row, bit, algorithms[input->type]);
+      }
     }
-
-    pthread_exit(NULL);
+  }
 }
 
 
 
 int main(int argc, char** argv) {
-    long t1, t2;
+    long t1, t2, thread;
     t1 = time(NULL);
 
-    stbi_set_flip_vertically_on_load(0);
+    stbi_set_flip_vertically_on_load(0); 
     if (argc != 3) return Usage();
     char* fileName = argv[1];
     if (!strcmp(argv[1], "pic4.jpg") && !strcmp(argv[2], "gauss")) {
@@ -65,7 +54,7 @@ int main(int argc, char** argv) {
     }
     enum KernelTypes type = GetKernelType(argv[2]);
 
-    Image srcImage, destImage, bwImage;
+    Image srcImage, destImage, bwImage;   
     srcImage.data = stbi_load(fileName, &srcImage.width, &srcImage.height, &srcImage.bpp, 0);
     if (!srcImage.data) {
         printf("Error loading file %s.\n", fileName);
@@ -76,28 +65,19 @@ int main(int argc, char** argv) {
     destImage.width = srcImage.width;
     destImage.data = malloc(sizeof(uint8_t) * destImage.width * destImage.bpp * destImage.height);
 
-    // Create an array to hold thread structures
-    pthread_t threads[NUM_THREADS];
-
-    // Create an array to hold thread arguments
-    ThreadArgs thread_args[NUM_THREADS];
-
-       for (int i = 0; i < NUM_THREADS; i++) {
-        thread_args[i].thread_id = i;
-        thread_args[i].srcImage = &srcImage;
-        thread_args[i].destImage = &destImage;
-        thread_args[i].type = type; // Pass the type as an argument
-
-        int result = pthread_create(&threads[i], NULL, threadConvolute, (void*)&thread_args[i]);
-        if (result) {
-            printf("Error creating thread %d\n", i);
-            return -1;
-        }
+    pthread_t* thread_handlers = (pthread_t*) malloc(NUM_THREADS * sizeof(pthread_t));
+    for(thread = 0; thread < NUM_THREADS; thread++){
+      inputStruct *params = (inputStruct*)malloc(sizeof(inputStruct));
+      params->srcImage = &srcImage;
+      params->destImage = &destImage;
+      params->type = type;
+      params->rank = thread;
+      pthread_create(&thread_handlers[thread], NULL, &threaded_convolute, (void*)params);
     }
-
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
+    for (thread = 0; thread < NUM_THREADS; thread++){
+        pthread_join(thread_handlers[thread], NULL);
     }
+    free(thread_handlers);
 
     stbi_write_png("output.png", destImage.width, destImage.height, destImage.bpp, destImage.data, destImage.bpp * destImage.width);
     stbi_image_free(srcImage.data);
@@ -105,5 +85,5 @@ int main(int argc, char** argv) {
     free(destImage.data);
     t2 = time(NULL);
     printf("Took %ld seconds\n", t2 - t1);
-    return 0;
+   return 0;
 }
