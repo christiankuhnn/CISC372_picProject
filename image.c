5518fc5a5b9188a382c4d6a2fc2169c3690961b4
christiankuhnn <kuhnc@udel.edu>
2023-10-30 21:31:50 -0400

    hello

--- image.c
+++ image.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#include <pthread.h>
 #include "image.h"
+#include <pthread.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -12,67 +11,41 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-// An array of kernel matrices to be used for image convolution.
-// The indexes of these match the enumeration from the header file.
-Matrix algorithms[] = {
-    {{0, -1, 0}, {-1, 4, -1}, {0, -1, 0}},
-    {{0, -1, 0}, {-1, 5, -1}, {0, -1, 0}},
-    {{1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0}, {1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0}, {1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0}},
-    {{1.0 / 16, 1.0 / 8, 1.0 / 16}, {1.0 / 8, 1.0 / 4, 1.0 / 8}, {1.0 / 16, 1.0 / 8, 1.0 / 16}},
-    {{-2, -1, 0}, {-1, 1, 1}, {0, 1, 2}},
-    {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}
+#define NUM_THREADS 4
+
+pthread_t threads[NUM_THREADS];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Define the kernel matrices as double arrays
+double algorithms[][3][3] = {
+    {{0.0, -1.0, 0.0}, {-1.0, 4.0, -1.0}, {0.0, -1.0, 0.0}},
+    {{0.0, -1.0, 0.0}, {-1.0, 5.0, -1.0}, {0.0, -1.0, 0.0}},
+    {{1.0/9.0, 1.0/9.0, 1.0/9.0}, {1.0/9.0, 1.0/9.0, 1.0/9.0}, {1.0/9.0, 1.0/9.0, 1.0/9.0}},
+    {{1.0/16.0, 1.0/8.0, 1.0/16.0}, {1.0/8.0, 1.0/4.0, 1.0/8.0}, {1.0/16.0, 1.0/8.0, 1.0/16.0}},
+    {{-2.0, -1.0, 0.0}, {-1.0, 1.0, 1.0}, {0.0, 1.0, 2.0}},
+    {{0.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 0.0}}
 };
 
-// Mutex for thread synchronization
-pthread_mutex_t mutex;
-
-// Thread data structure
 typedef struct {
-    Image* srcImage;
-    Image* destImage;
+    int thread_id;
     int start_row;
     int end_row;
-    enum KernelTypes type;
+    Image* srcImage;
+    Image* destImage;
+    double (*algorithm)[3][3];
 } ThreadData;
 
-// getPixelValue - Computes the value of a specific pixel on a specific channel using the selected convolution kernel
-uint8_t getPixelValue(Image* srcImage, int x, int y, int bit, Matrix algorithm) {
-    int px, mx, py, my;
-    uint8_t result = 0;
-    int span = srcImage->width * srcImage->bpp;
-
-    for (int i = -1; i <= 1; i++) {
-        for (int j = -1; j <= 1; j++) {
-            px = x + j;
-            py = y + i;
-            mx = x - j;
-            my = y - i;
-
-            if (mx < 0) mx = 0;
-            if (my < 0) my = 0;
-            if (px >= srcImage->width) px = srcImage->width - 1;
-            if (py >= srcImage->height) py = srcImage->height - 1;
-
-            result += algorithm[i + 1][j + 1] * srcImage->data[Index(mx, my, srcImage->width, bit, srcImage->bpp)];
-        }
-    }
-
-    return result;
-}
-
-// Modified convolute function for multi-threading
-void* applyConvolution(void* arg) {
+void* processImage(void* arg) {
     ThreadData* data = (ThreadData*)arg;
-    int start_row = data->start_row;
-    int end_row = data->end_row;
-    Image* srcImage = data->srcImage;
-    Image* destImage = data->destImage;
-    enum KernelTypes type = data->type;
 
-    for (int row = start_row; row < end_row; row++) {
-        for (int pix = 0; pix < srcImage->width; pix++) {
-            for (int bit = 0; bit < srcImage->bpp; bit++) {
-                destImage->data[Index(pix, row, srcImage->width, bit, srcImage->bpp)] = getPixelValue(srcImage, pix, row, bit, algorithms[type]);
+    for (int row = data->start_row; row < data->end_row; row++) {
+        for (int pix = 0; pix < data->srcImage->width; pix++) {
+            for (int bit = 0; bit < data->srcImage->bpp; bit++) {
+                uint8_t pixelValue = getPixelValue(data->srcImage, pix, row, bit, *(data->algorithm));
+
+                pthread_mutex_lock(&mutex);
+                data->destImage->data[Index(pix, row, data->srcImage->width, bit, data->srcImage->bpp)] = pixelValue;
+                pthread_mutex_unlock(&mutex);
             }
         }
     }
@@ -80,79 +53,53 @@ void* applyConvolution(void* arg) {
     pthread_exit(NULL);
 }
 
-// Implementation of Usage function
-int Usage() {
-    printf("Usage: image <filename> <type>\n\twhere type is one of (edge, sharpen, blur, gauss, emboss, identity)\n");
-    return -1;
-}
-
-// Implementation of GetKernelType function
-enum KernelTypes GetKernelType(char* type) {
-    if (!strcmp(type, "edge")) return EDGE;
-    else if (!strcmp(type, "sharpen")) return SHARPEN;
-    else if (!strcmp(type, "blur")) return BLUR;
-    else if (!strcmp(type, "gauss")) return GAUSE_BLUR;
-    else if (!strcmp(type, "emboss")) return EMBOSS;
-    else return IDENTITY;
-}
-
-// ... Other functions and variable declarations ...
-
 int main(int argc, char** argv) {
     long t1, t2;
     t1 = time(NULL);
 
     stbi_set_flip_vertically_on_load(0);
     if (argc != 3) return Usage();
+
     char* fileName = argv[1];
-    if (!strcmp(argv[1], "pic4.jpg") && !strcmp(argv[2], "gauss")) {
-        printf("You have applied a gaussian filter to Gauss which has caused a tear in the time-space continuum.\n");
-    }
     enum KernelTypes type = GetKernelType(argv[2]);
 
-    Image srcImage, destImage, bwImage;
+    Image srcImage, *destImage;
     srcImage.data = stbi_load(fileName, &srcImage.width, &srcImage.height, &srcImage.bpp, 0);
+
     if (!srcImage.data) {
         printf("Error loading file %s.\n", fileName);
         return -1;
     }
-    destImage.bpp = srcImage.bpp;
-    destImage.height = srcImage.height;
-    destImage.width = srcImage.width;
-    destImage.data = malloc(sizeof(uint8_t) * destImage.width * destImage.bpp * destImage.height);
 
-    // Initialize the mutex
-    pthread_mutex_init(&mutex, NULL);
+    destImage = (Image*)malloc(sizeof(Image));
+    destImage->bpp = srcImage.bpp;
+    destImage->height = srcImage.height;
+    destImage->width = srcImage.width;
+    destImage->data = (uint8_t*)malloc(sizeof(uint8_t) * destImage->width * destImage->bpp * destImage->height);
 
-    // Split the image into equal parts for multi-threading
-    int num_threads = 4;
-    int rows_per_thread = srcImage.height / num_threads;
-    pthread_t threads[num_threads];
-    ThreadData thread_data[num_threads];
+    int rows_per_thread = srcImage.height / NUM_THREADS;
 
-    // Create and manage threads
-    for (int i = 0; i < num_threads; i++) {
-        thread_data[i].srcImage = &srcImage;
-        thread_data[i].destImage = &destImage;
-        thread_data[i].start_row = i * rows_per_thread;
-        thread_data[i].end_row = (i == num_threads - 1) ? srcImage.height : (i + 1) * rows_per_thread;
-        thread_data[i].type = type;
+    for (int i = 0; i < NUM_THREADS; i++) {
+        ThreadData* thread_data = (ThreadData*)malloc(sizeof(ThreadData));
+        thread_data->thread_id = i;
+        thread_data->start_row = i * rows_per_thread;
+        thread_data->end_row = (i == NUM_THREADS - 1) ? srcImage.height : (i + 1) * rows_per_thread;
+        thread_data->srcImage = &srcImage;
+        thread_data->destImage = destImage;
+        thread_data->algorithm = algorithms[type];
 
-        pthread_create(&threads[i], NULL, applyConvolution, &thread_data[i]);
+        pthread_create(&threads[i], NULL, processImage, (void*)thread_data);
     }
 
-    // Wait for threads to finish
-    for (int i = 0; i < num_threads; i++) {
+    for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    // Cleanup the mutex
-    pthread_mutex_destroy(&mutex);
-
-    stbi_write_png("output.png", destImage.width, destImage.height, destImage.bpp, destImage.data, destImage.bpp * destImage.width);
+    stbi_write_png("output.png", destImage->width, destImage->height, destImage->bpp, destImage->data, destImage->bpp * destImage->width);
     stbi_image_free(srcImage.data);
 
-    free(destImage.data);
+    free(destImage->data);
+    free(destImage);
     t2 = time(NULL);
     printf("Took %ld seconds\n", t2 - t1);
     return 0;
