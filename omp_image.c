566e448d5d7a8f3bff9f79adc222e062c446da69
christiankuhnn <kuhnc@udel.edu>
2023-10-31 20:00:26 -0400

    hello

--- omp_image.c
+++ omp_image.c
@@ -2,19 +2,74 @@
 #include <stdint.h>
 #include <time.h>
 #include <string.h>
-#include <omp.h>
 #include "image.h"
+#include <omp.h> // Include OpenMP header
+
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
 Matrix algorithms[] = {
     {{0, -1, 0}, {-1, 4, -1}, {0, -1, 0}},
     {{0, -1, 0}, {-1, 5, -1}, {0, -1, 0}},
-    {{1/9.0, 1/9.0, 1/9.0}, {1/9.0, 1/9.0, 1/9.0}, {1/9.0, 1/9.0, 1/9.0}},
-    {{1.0/16, 1.0/8, 1.0/16}, {1.0/8, 1.0/4, 1.0/8}, {1.0/16, 1.0/8, 1.0/16}},
+    {{1 / 9.0, 1 / 9.0, 1 / 9.0}, {1 / 9.0, 1 / 9.0, 1 / 9.0}, {1 / 9.0, 1 / 9.0, 1 / 9.0}},
+    {{1.0 / 16, 1.0 / 8, 1.0 / 16}, {1.0 / 8, 1.0 / 4, 1.0 / 8}, {1.0 / 16, 1.0 / 8, 1.0 / 16}},
     {{-2, -1, 0}, {-1, 1, 1}, {0, 1, 2}},
     {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}
 };
+
+uint8_t getPixelValue(Image* srcImage, int x, int y, int bit, Matrix algorithm) {
+    int px, mx, py, my, i, span;
+    span = srcImage->width * srcImage->bpp;
+    px = x + 1;
+    py = y + 1;
+    mx = x - 1;
+    my = y - 1;
+    if (mx < 0) mx = 0;
+    if (my < 0) my = 0;
+    if (px >= srcImage->width) px = srcImage->width - 1;
+    if (py >= srcImage->height) py = srcImage->height - 1;
+    uint8_t result =
+        algorithm[0][0] * srcImage->data[Index(mx, my, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[0][1] * srcImage->data[Index(x, my, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[0][2] * srcImage->data[Index(px, my, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[1][0] * srcImage->data[Index(mx, y, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[1][1] * srcImage->data[Index(x, y, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[1][2] * srcImage->data[Index(px, y, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[2][0] * srcImage->data[Index(mx, py, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[2][1] * srcImage->data[Index(x, py, srcImage->width, bit, srcImage->bpp)] +
+        algorithm[2][2] * srcImage->data[Index(px, py, srcImage->width, bit, srcImage->bpp)];
+    return result;
+}
+
+void convolute(Image* srcImage, Image* destImage, Matrix algorithm) {
+    // OMP: Parallelize the outer loop using OpenMP
+    #pragma omp parallel for
+    for (int row = 0; row < srcImage->height; row++) {
+        for (int pix = 0; pix < srcImage->width; pix++) {
+            for (int bit = 0; bit < srcImage->bpp; bit++) {
+                destImage->data[Index(pix, row, srcImage->width, bit, srcImage->bpp)] =
+                    getPixelValue(srcImage, pix, row, bit, algorithm);
+            }
+        }
+    }
+}
+
+int Usage() {
+    printf("Usage: image <filename> <type>\n\twhere type is one of (edge, sharpen, blur, gauss, emboss, identity)\n");
+    return -1;
+}
+
+enum KernelTypes GetKernelType(char* type) {
+    if (!strcmp(type, "edge")) return EDGE;
+    else if (!strcmp(type, "sharpen")) return SHARPEN;
+    else if (!strcmp(type, "blur")) return BLUR;
+    else if (!strcmp(type, "gauss")) return GAUSE_BLUR;
+    else if (!strcmp(type, "emboss")) return EMBOSS;
+    else return IDENTITY;
+}
 
 int main(int argc, char** argv) {
     long t1, t2;
@@ -28,7 +83,7 @@ int main(int argc, char** argv) {
     }
     enum KernelTypes type = GetKernelType(argv[2]);
 
-    Image srcImage, destImage, bwImage;
+    Image srcImage, destImage;
     srcImage.data = stbi_load(fileName, &srcImage.width, &srcImage.height, &srcImage.bpp, 0);
     if (!srcImage.data) {
         printf("Error loading file %s.\n", fileName);
@@ -39,22 +94,30 @@ int main(int argc, char** argv) {
     destImage.width = srcImage.width;
     destImage.data = malloc(sizeof(uint8_t) * destImage.width * destImage.bpp * destImage.height);
 
+    // OMP: Initialize OpenMP and set the number of threads
     #pragma omp parallel
     {
-        #pragma omp for
-        for (int row = 0; row < srcImage.height; row++) {
-            for (int pix = 0; pix < srcImage.width; pix++) {
-                for (int bit = 0; bit < srcImage.bpp; bit++) {
-                    destImage.data[Index(pix, row, srcImage.width, bit, srcImage.bpp)] =
-                        getPixelValue(&srcImage, pix, row, bit, algorithms[type]);
-                }
-            }
+        #pragma omp master
+        {
+            int numThreads = omp_get_num_threads();
+            printf("Number of threads: %d\n", numThreads);
         }
+    }
+
+    // OMP: Parallelize the loop that creates threads
+    #pragma omp parallel for
+    for (long thread = 0; thread < NUM_THREADS; thread++) {
+        inputStruct params;
+        params.srcImage = &srcImage;
+        params.destImage = &destImage;
+        params.type = type;
+        params.rank = thread;
+        threaded_convolute(&params);
     }
 
     stbi_write_png("output.png", destImage.width, destImage.height, destImage.bpp, destImage.data, destImage.bpp * destImage.width);
     stbi_image_free(srcImage.data);
-
+    
     free(destImage.data);
     t2 = time(NULL);
     printf("Took %ld seconds\n", t2 - t1);
